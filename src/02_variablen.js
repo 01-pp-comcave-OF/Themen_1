67c21b2e59d361b2520636235fcfe2b6c4a50eb4
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-09 11:25:46 +0000

    wertzuweisen II + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -12,9 +12,32 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max";  // wertzuweisung | assignment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // wertzuweisung | assignment
-console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max";  // wertzuweisung | assignment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // wertzuweisung | assignment
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+
+/* 
+wertzuweisen II + Datentypen
+JS ist eine untypisierte Sprache! 
+ */
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("inhalt: " + test);
+console.log("-------------");
+test = "hello"; // string
+console.log("Datentyp: " + typeof test);
+console.log("inhalt: " + test);
+console.log("-------------");
+test = 2; // number
+console.log("Datentyp: " + typeof test);
+console.log("inhalt: " + test);
+console.log("-------------");
+test = true;
+console.log("Datentyp: " + typeof test);
+console.log("inhalt: " + test);
+
+
