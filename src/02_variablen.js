fc34a4dd05d43d99ebe2e33e406d3748076a53af
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-09 10:51:20 +0000

    02/ Deklaration + Wertzuweisung

---
+++ src/02_variablen.js
@@ -0,0 +1,20 @@
+
+// Zeilenkommentar
+
+/*
+    Block-Kommentar
+*/
+
+/***** Variablen 01 *******/5
+
+// console.log("Hi!"); // Ausgabe 
+// console.log(Hi); // Ausgabe 
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max";  // wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // wertzuweisung | assignment
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
