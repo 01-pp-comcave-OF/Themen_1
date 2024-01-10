ea801c876f8e817ab624a855cb7129d3c3d7603d
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-10 11:33:39 +0000

    05/berechnung variante 1

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,4 +3,15 @@
 
 // Deklaration
 let ageJohn, ageMark;
+const birthYearJohn = 2000
+const birthYearMark = 1990
+
+// Berechnung Alter
+ageJohn = 2024 - birthYearJohn;
+ageMark = 2024 - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
 
