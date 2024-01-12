d9499ad38be6a0548b6730b2d8015ffd28e2a10c
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-12 09:14:50 +0000

    08_Kapselung von Codeblöcken

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,12 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+test();
+
+// Funktionsrumpf | calle
+// Funktionsdeklaration
+function test()
+{
+console.log(2);
+}
