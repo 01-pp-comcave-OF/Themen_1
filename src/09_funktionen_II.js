f6a0545b2aea91e9818f76f3d496e66d51ef1114
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-15 11:15:35 +0000

    09/calculator ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -11,14 +11,52 @@
 */
 
 const ERROR_STR_DIV = "Davision durch Null ist nicht definiert";
+const ERROR_STR_GEN = "Irgendwas ging schief";
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op){
+	switch (op) {
+		case "+":  			// add()
+			return	add(a,b);
+		case "-":  			// sub()
+			return	subtract(a,b);
+		case "*":  			// mul()
+			return	multiply(a,b);
+		case ":":		    // div()
+		case "/":			// div()
+			return	divide(a,b);
+		default:			// Error
+			return ERROR_STR_GEN;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // module: division a / b |  test:
-output (divide(4,2));
-output (divide(3,2));
-output (divide(3,-2));
-output (divide(0,2));
-output (divide(3,0));
-output (divide(0,0));
+// output (divide(4,2));
+// output (divide(3,2));
+// output (divide(3,-2));
+// output (divide(0,2));
+// output (divide(3,0));
+// output (divide(0,0));
 function divide(a,b){
 
 if(b == 0){
@@ -36,9 +74,9 @@ if(b == 0){
 // output(multiply(3,2));
 // output(multiply(3,-2));
 // output(multiply(3,0));
-// function multiply(a,b) {
-// 	return a / b;
-// }
+function multiply(a,b) {
+	return a / b;
+}
 
 
 // module: subtraction a - b |  test:
@@ -46,18 +84,18 @@ if(b == 0){
 // output(subtract(3,-2));
 // output(subtract(3,0));
 // output(subtract(0,2));
-// function subtract(a,b) {
-// 	return a - b;
-// }
+function subtract(a,b) {
+	return a - b;
+}
 
 
 // // module: addition a + b |  test:
 // output(add(2,2));
 // output(add(2,-2));
 // output(add(2,0));
-// function add (a,b) {
-// 	return a + b;
-// }
+function add (a,b) {
+	return a + b;
+}
 
 
 // module: output | test:
