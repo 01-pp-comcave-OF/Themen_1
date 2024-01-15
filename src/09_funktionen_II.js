02bd50260221710c6377e0f920d24be6669cc6ae
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-15 09:50:50 +0000

    09/function_ divide/multi/add/sub variable 1

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,25 +10,50 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: division a / b |  test:
+output (divide(4,2));
+output (divide(3,2));
+output (divide(3,-2));
+output (divide(0,2));
+output (divide(3,0));
+function divide(a,b){
+
+	if (b !== 0) {  // b ungleich 0
+		return a / b;
+	}else {
+		return "Davision durch Null ist nicht definiert";
+	}
+}
+
+
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+// function multiply(a,b) {
+// 	return a / b;
+// }
 
 
 // module: subtraction a - b |  test:
-output(subtract(3,2));
-output(subtract(3,-2));
-output(subtract(3,0));
-output(subtract(0,2));
-function subtraction(a,b) {
-	return a - b;
-}
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+// function subtract(a,b) {
+// 	return a - b;
+// }
 
 
-// module: addition a + b |  test:
+// // module: addition a + b |  test:
 // output(add(2,2));
 // output(add(2,-2));
 // output(add(2,0));
-function add (a,b) {
-	return a + b;
-}
+// function add (a,b) {
+// 	return a + b;
+// }
 
 
 // module: output | test:
