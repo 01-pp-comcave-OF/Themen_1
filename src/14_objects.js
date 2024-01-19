ea6c62767699f4c745abd9abdd726791bc3f804e
Firasalzaidi <155629441+Firasalzaidi@users.noreply.github.com>
2024-01-19 09:18:25 +0000

    14 / Objekte 1 Daten/Funktionen

--- src/14_objects.js
+++ src/14_objects.js
@@ -1,32 +1,57 @@
 /***** Erinnerung: Arrays *******/
  
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
 // // nested arrays - Prinzip: [[[]]]
 // // nested arrays  - hier: [[],[]]
-arr =   [
-                ["Ich","bin","Max"],
-                ["Ich","bin","Moritz"]
-        ];
+// arr =   [
+//                 ["Ich","bin","Max"],
+//                 ["Ich","bin","Moritz"]
+//         ];
  
-output(arr);
-output(arr[0]);
-output(arr[0][0]+ " " + arr[1][2]);
-output(arr[1][0]);
-output("------------");
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
 
 // nested loop
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        output("Index i: " + i + " Index j: " + j);
-        output(arr[i][j]);      
-    }
-}
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);      
+//     }
+// }
 
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+                firstName:"Firas",
+                familyName:"Al-Zaidi", 
+                salary: [120000, 160000],
+                permission: true,
+                sayHello: function(){
+                    // return "Hallo, ich bin"
+                    return "Hello ,ich bin " + this.firstName;
+                }
+            };
+
+
+// output(person);
+output(person.familyName)
+output(person.firstName)
+output(person.salary[1])
+output(person.permission)
+output(person.sayHello())
+
+const txt = person.sayHello() + " Ich bin " + person.firstName + " und verdiene " + person.salary[1] + " p.a."
+
+output(txt);
 
 
 
